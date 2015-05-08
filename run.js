c2b10253d12a5ba6bb7e34491930bee0ef791bce
Dmitry Nazarov <mail@mkn8rd.org>
2015-05-08 20:28:11 +0300

    ..

--- run.js
+++ run.js
@@ -1,3 +1,20 @@
-var data = p;
+// connect("ws://45.33.103.22:443")
+// setNick("mkn8rd")
 
-console.log(data[0].y);
+var data = p; // breakpoint on 500 in main_out.js
+
+// =============================
+
+setInterval(function () {
+    run();
+}, 50);
+
+function logArrayElements(element, index, array) {
+    sizes += element.size + ' ';
+}
+
+function run() {
+    sizes = [];
+    data.forEach(logArrayElements);
+    console.log(sizes); // t0d0 make it to div in body instead
+}
