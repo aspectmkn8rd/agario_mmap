4d86b7614a5c0aa0dc9804905df322056e2c9013
Dmitry Nazarov <mail@mkn8rd.org>
2015-05-08 23:45:47 +0300

    mmap canvas now displaying

--- run.js
+++ run.js
@@ -1,5 +1,30 @@
 var data = p; // breakpoint on :500 in main_out.js
 
+function cloneCanvas(old_canvas) {
+    var new_canvas = document.createElement('canvas');
+    var ctx = new_canvas.getContext('2d');
+    var proportion = 7;
+    new_canvas.width = old_canvas.width / proportion;
+    new_canvas.height = new_canvas.width;
+    ctx.drawImage(old_canvas, 0, 0);
+    return new_canvas;
+}
+
+// mmap canvas
+if (typeof main_canvas !== 'undefined') {
+    document.body.removeChild(mmap_canvas);
+}
+
+var main_canvas = document.getElementById('canvas');
+var mmap_canvas = cloneCanvas(main_canvas);
+mmap_canvas.id = 'mmap_canvas';
+mmap_canvas.style.position = 'fixed';
+mmap_canvas.style.right = 0;
+mmap_canvas.style.bottom = 0;
+mmap_canvas.style.whiteSpace = 'pre';
+document.body.appendChild(mmap_canvas);
+
+// =====================================================
 // onscrean console
 if (typeof onscrean !== 'undefined') {
 }
