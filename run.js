d4d4bb6e2b11b8a060f40e5baa518836ea69c5cd
Dmitry Nazarov <mail@mkn8rd.org>
2015-05-09 04:00:50 +0300

    basic mmap f-nal works

--- run.js
+++ run.js
@@ -1,26 +1,20 @@
 var data = p; // breakpoint on :500 in main_out.js
 
-function cloneCanvas(old_canvas) {
-    var new_canvas = document.createElement('canvas');
-    var ctx = new_canvas.getContext('2d');
-    var proportion = 7;
-    new_canvas.width = old_canvas.width / proportion;
-    new_canvas.height = new_canvas.width;
-    ctx.drawImage(old_canvas, 0, 0);
-    return new_canvas;
-}
-
 // mmap canvas
 if (typeof main_canvas !== 'undefined') {
     document.body.removeChild(mmap_canvas);
 }
 
+var mmap_proportion = 5;
 var main_canvas = document.getElementById('canvas');
-var mmap_canvas = cloneCanvas(main_canvas);
+var mmap_canvas = document.createElement('canvas');
+mmap_canvas.width = window.innerWidth / mmap_proportion;
+mmap_canvas.height = window.innerWidth / mmap_proportion;
 mmap_canvas.id = 'mmap_canvas';
+mmap_canvas.style.border = "black 1px solid";
 mmap_canvas.style.position = 'fixed';
-mmap_canvas.style.right = 0;
-mmap_canvas.style.bottom = 0;
+mmap_canvas.style.right = 10;
+mmap_canvas.style.bottom = 10;
 mmap_canvas.style.whiteSpace = 'pre';
 document.body.appendChild(mmap_canvas);
 
@@ -54,18 +48,33 @@ var interval_id = setInterval(function () {
 }, 50);
 interval_ids.push(interval_id);
 // =====================================================
+function draw_mmap(my, map) {
+    var ctx = mmap_canvas.getContext('2d');
+    ctx.fillStyle = "#F2FBFF";
+    ctx.fillRect(0, 0, mmap_canvas.width, mmap_canvas.width);
 
+    ctx.beginPath();
+    pr_x = my.x / map.x;
+    pr_y = my.y / map.y;
+    ctx.arc(pr_x * mmap_canvas.width, pr_y * mmap_canvas.height / 2, 3, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+// =====================================================
 function run() {
     setRegion("SETTINGS_REGION");
     setNick("SETTINGS_NICKNAME");
     setShowMass("SETTINGS_SHOW_MASS");
+
     var map = {
         'x': K,
         'y': L
     };
     var my = m[0];
+
     onscrean.innerText = 'x: ' + my.x + ' / ' + map.x;
     onscrean.innerText += '\n';
     onscrean.innerText += 'y: ' + my.y + ' / ' + map.y;
     onscrean.innerText += '\n';
+
+    draw_mmap(my, map);
 }
